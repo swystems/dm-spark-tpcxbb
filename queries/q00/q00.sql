4ce4aeed885e11a989e47d16d0442ef0322644c0
Jeferson Morales Mariciano <jeferson.morales.mariciano@gmail.com>
2024-04-24 11:31:05 +0200

    style: further exercise info
    Signed-off-by: Jeferson Morales Mariciano <jeferson.morales.mariciano@gmail.com>

--- queries/q00/q00.sql
+++ queries/q00/q00.sql
@@ -38,7 +38,7 @@
 -- There are are several ways to to "basketing". Implemented is way A)
 -- A) collect distinct viewed items per session (same sales_sk) in list and employ a UDTF to produce pairwise combinations of all list elements
 -- B) distribute by sales_sk end employ reducer streaming script to aggregate all items per session and produce the pairs
--- C) pure SQL: produce pairings by self joining on sales_sk and filtering out left.item_sk < right.item_sk (elimiates dupplicates and switched posistions)
+-- C) pure SQL: produce pairings by self joining on sales_sk and filtering out left.item_sk < right.item_sk (eliminate duplicates and switched positions)
 
 
 -- Resources
@@ -61,6 +61,7 @@ STORED AS ${env:BIG_BENCH_spark_sql_default_fileformat_result_table} LOCATION '$
 
 -- the real query part
 -- Find the most frequent ones
+-- consult file `queries/queryParameters.sql` for q01_xxxxxx argument variables
 
 INSERT INTO TABLE ${RESULT_TABLE}
 SELECT i_category, COUNT(*)
